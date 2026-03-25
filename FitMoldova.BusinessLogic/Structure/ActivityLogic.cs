940e0dad235e554e18c4cb88ec701f4abc242939
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:46:13 +0200

    Implementarea metodelor din interfețe pentru Activity, Event, Post..

---
+++ FitMoldova.BusinessLogic/Structure/ActivityLogic.cs
@@ -0,0 +1,15 @@
+﻿using FitMoldova.BusinessLogic.Core;
+using FitMoldova.BusinessLogic.Interfaces;
+using FitMoldova.Domain.Models.Activity;
+using FitMoldova.Domain.Models.Services;
+
+namespace FitMoldova.BusinessLogic.Structure
+{
+     public class ActivityLogic : ActivityAction, IActivityLogic
+     {
+          public ServiceResponse CreateActivity(ActivityCreateDto dto) => CreateActivityExecution(dto);
+          public ServiceResponse GetByUser(int userId) => GetByUserExecution(userId);
+          public ServiceResponse GetById(int id) => GetByIdExecution(id);
+          public ServiceResponse Delete(int id) => DeleteExecution(id);
+     }
+}
---
+++ FitMoldova.BusinessLogic/Structure/ChallengeLogic.cs
@@ -0,0 +1,16 @@
+﻿using FitMoldova.BusinessLogic.Core;
+using FitMoldova.BusinessLogic.Interfaces;
+using FitMoldova.Domain.Models.Challenge;
+using FitMoldova.Domain.Models.Services;
+
+namespace FitMoldova.BusinessLogic.Structure
+{
+     public class ChallengeLogic : ChallengeAction, IChallengeLogic
+     {
+          public ServiceResponse GetAll() => GetAllExecution();
+          public ServiceResponse GetById(int id) => GetByIdExecution(id);
+          public ServiceResponse CreateChallenge(ChallengeCreateDto dto) => CreateChallengeExecution(dto);
+          public ServiceResponse JoinChallenge(int cId, int uId) => JoinChallengeExecution(cId, uId);
+          public ServiceResponse Delete(int id) => DeleteExecution(id);
+     }
+}

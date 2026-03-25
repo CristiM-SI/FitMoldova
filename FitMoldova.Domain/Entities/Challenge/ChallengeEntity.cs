3365ba67e18661d52a19cb7c27fd472ce5d61d3d
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:37:16 +0200

    Chllenge Entity

---
+++ FitMoldova.Domain/Entities/Challenge/ChallengeEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMoldova.Domain.Entities.Challenge
+{
+     public class ChallengeEntity
+     {
+          [Key]
+          [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+          public int Id { get; set; }
+
+          [Required]
+          [StringLength(80)]
+          public string Name { get; set; } = string.Empty;
+
+          public string Description { get; set; } = string.Empty;
+
+          [StringLength(20)]
+          public string Duration { get; set; } = string.Empty;
+
+          [StringLength(10)]
+          public string Difficulty { get; set; } = string.Empty;
+
+          public int Participants { get; set; } = 0;
+     }
+}

878b1f6cd0190effb895146324f3ba31f676fa55
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:47:41 +0200

    Controllere User și Health

---
+++ FitMoldova.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Template;
+
+namespace FitMoldova.Api.Controllers
+{
+     [Route("api/[controller]")]
+     [ApiController]
+     public class HealthController : ControllerBase
+     {
+          [HttpGet(template: "check")] 
+
+        public IActionResult Check()
+          {
+
+               return Ok("Server is up and running");
+          }
+     }
+}

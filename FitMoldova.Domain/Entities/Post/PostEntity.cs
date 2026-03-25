899789a43c42e58fa2e6af87e2681cce8d2d0d38
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:38:30 +0200

    Event, Postare și Trasee, entități

---
+++ FitMoldova.Domain/Entities/Post/PostEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMoldova.Domain.Entities.Post
+{
+     public class PostEntity
+     {
+          [Key]
+          [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+          public int Id { get; set; }
+
+          public int UserId { get; set; }
+
+          [Required]
+          public string Content { get; set; } = string.Empty;
+
+          [StringLength(40)]
+          public string Sport { get; set; } = string.Empty;
+
+          public int Likes { get; set; } = 0;
+          public int CommentsCount { get; set; } = 0;
+          public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+     }
+}

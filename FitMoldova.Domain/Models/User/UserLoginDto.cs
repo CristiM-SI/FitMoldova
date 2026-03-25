a0a273cf1d94a4ffaf579979856e4574b7e641f8
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:39:07 +0200

    Dto pt User

---
+++ FitMoldova.Domain/Models/User/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FitMoldova.Domain.Models.User
+{
+     public class UserLoginDto
+     {
+          public string Credential { get; set; } = string.Empty;
+          public string Password { get; set; } = string.Empty;
+     }
+}

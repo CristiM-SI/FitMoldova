4798b27cace8866b0345b15906fdc95aadd484f1
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:44:22 +0200

    Proiectul BusinessLogic și registrarea

---
+++ FitMoldova.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using FitMoldova.BusinessLogic.Interfaces;
+using FitMoldova.BusinessLogic.Structure;
+
+namespace FitMoldova.BusinessLogic
+{
+     public class BusinessLogic
+     {
+          public BusinessLogic() { }
+
+          public IUserLogic UserLogic() => new UserLogic();
+          public IActivityLogic ActivityLogic() => new ActivityLogic();
+          public IRouteLogic RouteLogic() => new RouteLogic();
+          public IEventLogic EventLogic() => new EventLogic();
+          public IClubLogic ClubLogic() => new ClubLogic();
+          public IChallengeLogic ChallengeLogic() => new ChallengeLogic();
+          public IPostLogic PostLogic() => new PostLogic();
+     }
+}

3c3fa0a453529d41fba31b0f022c04946d563c65
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-25 12:42:25 +0200

    Adăugarea sesiunii bazei de date, și a proiectului DataAcces

---
+++ DataAccesLayer/DbSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitMoldova.DataAccesLayer;
+
+namespace FitMoldova.DataAccesLayer
+{
+     public class DbSession
+     {
+          public FitMoldovaContext FitMoldovaContext()
+          {
+               return new FitMoldovaContext();
+          }
+     }
+}

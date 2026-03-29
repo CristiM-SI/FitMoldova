b0473c484236bfc31f777506252f4114d62506a5
Ovidiu Crestin <ovidiucrestin@gmail.com>
2026-03-29 13:25:56 +0300

    Ștergere back incorect

--- FitMoldova.Domain/Models/User/UserLoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FitMoldova.Domain.Models.User
-{
-     public class UserLoginDto
-     {
-          public string Credential { get; set; } = string.Empty;
-          public string Password { get; set; } = string.Empty;
-     }
-}
--- FitMoldova.Domain/Models/User/UserUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FitMoldova.Domain.Models.User
-{
-     public class UserUpdateDto
-     {
-          public string Username { get; set; } = string.Empty;
-          public string Email { get; set; } = string.Empty;
-     }
-}

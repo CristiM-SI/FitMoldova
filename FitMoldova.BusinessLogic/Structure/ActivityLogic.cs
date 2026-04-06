b721048df68477b2c1026fb1dfd893cac053716c
Daniel-C-420 <aguy4life32@gmail.com>
2026-04-06 13:50:42 +0300

    Activity update

--- FitMoldova.BusinessLogic/Structure/ActivityLogic.cs
+++ FitMoldova.BusinessLogic/Structure/ActivityLogic.cs
@@ -7,11 +7,12 @@ namespace FitMoldova.BusinessLogic.Structure
 {
      public class ActivityLogic : ActivityAction, IActivityLogic
      {
-          public ServiceResponse GetAll() => GetAllExecution();
-          public ServiceResponse GetById(int id) => GetByIdExecution(id);
-          public ServiceResponse CreateActivity(ActivityCreateDto dto) => CreateActivityExecution(dto);
-          public ServiceResponse Delete(int id) => DeleteExecution(id);
-          public ServiceResponse JoinActivity(int activityId, int userId) => JoinActivityExecution(activityId, userId);
-          public ServiceResponse GetParticipants(int activityId) => GetParticipantsExecution(activityId);
+          public ServiceResponse GetAll()                                         => GetAllExecution();
+          public ServiceResponse GetById(int id)                                  => GetByIdExecution(id);
+          public ServiceResponse CreateActivity(ActivityCreateDto dto)            => CreateActivityExecution(dto);
+          public ServiceResponse UpdateActivity(int id, ActivityUpdateDto dto)    => UpdateActivityExecution(id, dto);
+          public ServiceResponse Delete(int id)                                   => DeleteExecution(id);
+          public ServiceResponse JoinActivity(int activityId, int userId)         => JoinActivityExecution(activityId, userId);
+          public ServiceResponse GetParticipants(int activityId)                  => GetParticipantsExecution(activityId);
      }
 }
